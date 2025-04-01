16d37e96a0c806ef52f2a42b2fa4d36db2b88709
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-04-01 21:53:51 +0530

    feat: building

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,35 @@
-import { Text, View } from "react-native";
-import "./global.css"
+import { StatusBar, Text, TouchableOpacity, View } from "react-native";
+import "./global.css";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>Calculator app</Text>
+      <View className=""></View>
+      <View className="grid grid-cols-4 gap-2 p-2">
+        {[
+          "7",
+          "8",
+          "9",
+          "x",
+          "4",
+          "5",
+          "6",
+          "+",
+          "1",
+          "2",
+          "3",
+          "-",
+          "0",
+          ".",
+          "⌫",
+          "=",
+        ].map((item) => (
+          <TouchableOpacity
+            key={item}
+            className="h-20 rounded-full justify-center items-center  bg-gray-500"
+          />
+        ))}
+      </View>
     </View>
   );
 }
