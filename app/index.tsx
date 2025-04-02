1193e87721b2e9fe5685461846c4f13d501fe76b
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-04-02 12:33:14 +0530

    feat= for "C" and "prev"

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,43 @@
-import { StatusBar, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  StatusBar,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import "./global.css";
 import { useState } from "react";
 
 export default function Index() {
-
   const [equation, setEquation] = useState("");
   const [result, setResult] = useState("");
 
-  const handleClick = (value:string) => {
-    if(value== "="){
-      try{
-        setResult(eval(equation).toString())
+  const handleClick = (value: string) => {
+    if (value == "=") {
+      try {
+        setResult(eval(equation).toString());
+      } catch (error) {
+        setResult("Error");
       }
-      catch(error){
-        setResult("Error")
-      }
+    } else if (value == "C") {
+      setEquation("");
+      setResult("");
+    } else {
+      setEquation((prev) => prev + value);
     }
-  }
+  };
 
   return (
-
     <View className="flex-1 justify-center items-center w-full bg-black">
       <View>
-        <TextInput className="bg-gray-300 w-[300px] mb-5 rounded text-[25px]" value="0" textAlign="center" />
+        <TextInput
+          className="bg-gray-300 w-[300px] mb-5 rounded text-[25px]"
+          value="0"
+          textAlign="center"
+        />
         <View>{result}</View>
       </View>
-      <View className="flex-row flex-wrap w-full justify-center items-center " >
+      <View className="flex-row flex-wrap w-full justify-center items-center ">
         {[
           "7",
           "8",
@@ -41,13 +53,13 @@ export default function Index() {
           "-",
           "0",
           ".",
-          "⌫",
+          "C",
           "=",
         ].map((item) => (
           <TouchableOpacity
             key={item}
             className="h-20 w-20 m-2 rounded-full justify-center items-center bg-gray-500"
-            onPress={()=>handleClick(item)}
+            onPress={() => handleClick(item)}
           >
             <Text className="text-white text-3xl">{item}</Text>
           </TouchableOpacity>
